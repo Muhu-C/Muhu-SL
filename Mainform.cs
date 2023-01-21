a3419b38bc18b01490e90c84578e64988e88940e
Muhu-C <123132305+Muhu-C@users.noreply.github.com>
2023-01-21 17:49:47 +0800

    Update Mainform.cs

--- Mainform.cs
+++ Mainform.cs
@@ -16,16 +16,33 @@ namespace MMSL
     public partial class Mainform : Form
     {
         public About AboutForm = new About();
-        public static Color Backcolor = Color.Azure;
+
+        #region 优化
         public Mainform()
         {
+            MessageBox.Show("打开此程序时请关闭其他Java程序，以防数据丢失");
             InitializeComponent();
+        }
+        
+        protected override CreateParams CreateParams //防止界面闪烁
+        {
+            get
+            {
+                CreateParams paras = base.CreateParams;
+                paras.ExStyle |= 0x02000000;
+                return paras;
+            }
         }
 
         private void Mainform_Load(object sender, EventArgs e)
         {
-
+            this.DoubleBuffered = true;//设置本窗体
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true); // 禁止擦除背景.
+            SetStyle(ControlStyles.DoubleBuffer, true); // 双缓冲
         }
+        #endregion
+
 
         private void 关闭ToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -37,6 +54,7 @@ namespace MMSL
             PageA.Visible = true;
             PageB.Visible = false;
             PageC.Visible = false;
+            PageD.Visible = false;
         }
 
         private void ConsolePage_Click(object sender, EventArgs e)
@@ -44,6 +62,7 @@ namespace MMSL
             PageB.Visible = true;
             PageA.Visible = false;
             PageC.Visible = false;
+            PageD.Visible = false;
         }
 
         private void PluginsPage_Click(object sender, EventArgs e)
@@ -51,6 +70,15 @@ namespace MMSL
             PageC.Visible = true;
             PageA.Visible = false;
             PageB.Visible = false;
+            PageD.Visible = false;
+        }
+
+        private void DownloadPage_Click(object sender, EventArgs e)
+        {
+            PageD.Visible = true;
+            PageA.Visible = false;
+            PageB.Visible = false;
+            PageC.Visible = false;
         }
 
         private void 重启ToolStripMenuItem_Click(object sender, EventArgs e)
@@ -67,6 +95,5 @@ namespace MMSL
                 AboutForm.Show();
             }
         }
-
     }
 }
